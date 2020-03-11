82b049ed8a2011822449f64e38531674e3c684d8
Dimitrios Panteleimon Giakatos <dgiakatos@csd.auth.gr>
2020-03-11 23:15:10 +0200

    Allergic diseases and Hospital treatment tabs layout in user dashboard is ready

--- src/app/dashboard/user-dashboard/user-dashboard.component.ts
+++ src/app/dashboard/user-dashboard/user-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {faCalendarAlt} from '@fortawesome/free-solid-svg-icons';
+import {NgbCalendar, NgbDate, NgbDateParserFormatter} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-user-dashboard',
@@ -11,13 +12,52 @@ export class UserDashboardComponent implements OnInit {
   addExtraVaccination = false;
   model;
   faCalendarAlt = faCalendarAlt;
+  addAllergicDisease = false;
+  addHospitalTreatment = false;
+  hoveredDate: NgbDate;
+  fromDate: NgbDate;
+  toDate: NgbDate;
 
-  constructor() { }
+  constructor(calendar: NgbCalendar) {
+    this.fromDate = calendar.getToday();
+    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+  }
 
   ngOnInit(): void {
   }
 
   addVaccination() {
     this.addExtraVaccination = !this.addExtraVaccination;
+  }
+
+  addAllergic() {
+    this.addAllergicDisease = !this.addAllergicDisease;
+  }
+
+  addHospital() {
+    this.addHospitalTreatment = !this.addHospitalTreatment;
+  }
+
+  onDateSelection(date: NgbDate) {
+    if (!this.fromDate && !this.toDate) {
+      this.fromDate = date;
+    } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
+      this.toDate = date;
+    } else {
+      this.toDate = null;
+      this.fromDate = date;
+    }
+  }
+
+  isHovered(date: NgbDate) {
+    return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
+  }
+
+  isInside(date: NgbDate) {
+    return date.after(this.fromDate) && date.before(this.toDate);
+  }
+
+  isRange(date: NgbDate) {
+    return date.equals(this.fromDate) || date.equals(this.toDate) || this.isInside(date) || this.isHovered(date);
   }
 }
