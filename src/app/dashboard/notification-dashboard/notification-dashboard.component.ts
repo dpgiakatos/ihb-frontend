82ef96e60b52726224f83033ffe15706441678b3
Dimitrios Panteleimon Giakatos <dgiakatos@csd.auth.gr>
2020-03-12 21:36:24 +0200

    Notification dashboard complete

--- src/app/dashboard/notification-dashboard/notification-dashboard.component.ts
+++ src/app/dashboard/notification-dashboard/notification-dashboard.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {faFlag} from "@fortawesome/free-solid-svg-icons";
+
+interface Alert {
+  type: string;
+  message: string;
+}
+
+const ALERTS: Alert[] = [{
+  type: 'success',
+  message: 'This is an success alert',
+}, {
+  type: 'danger',
+  message: 'This is a danger alert',
+}
+];
 
 @Component({
   selector: 'app-notification-dashboard',
@@ -7,9 +22,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationDashboardComponent implements OnInit {
 
-  constructor() { }
+  alerts: Alert[];
+  faFlag = faFlag;
+
+  constructor() {
+    this.alerts = Array.from(ALERTS);
+  }
 
   ngOnInit(): void {
   }
 
+  close(alert: Alert) {
+    this.alerts.splice(this.alerts.indexOf(alert), 1);
+  }
 }
