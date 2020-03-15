cdc41ea26f00318168b4f347de5812d743cd1062
Dimitrios Panteleimon Giakatos <dgiakatos@csd.auth.gr>
2020-03-15 20:31:37 +0200

    scroll-top component added

---
+++ src/app/home-page/scroll-top/scroll-top.component.ts
@@ -0,0 +1,38 @@
+import {Component, HostListener, Inject, OnInit} from '@angular/core';
+import {faArrowUp} from "@fortawesome/free-solid-svg-icons";
+import {DOCUMENT} from "@angular/common";
+
+@Component({
+  selector: 'app-scroll-top',
+  templateUrl: './scroll-top.component.html',
+  styleUrls: ['./scroll-top.component.css']
+})
+export class ScrollTopComponent implements OnInit {
+
+  windowScrolled: boolean;
+  faArrowUp = faArrowUp;
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  ngOnInit(): void {
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+      this.windowScrolled = true;
+    }
+    else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+      this.windowScrolled = false;
+    }
+  }
+  scrollToTop() {
+    (function smoothscroll() {
+      var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - (currentScroll / 8));
+      }
+    })();
+  }
+}
