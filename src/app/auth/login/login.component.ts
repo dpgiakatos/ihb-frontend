35ae482b9cbdec1e9a11ec926209033136d015bd
Dimitrios Panteleimon Giakatos <dgiakatos@csd.auth.gr>
2020-04-11 20:12:01 +0300

    Validation complete and bug fixes

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  form;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      'email': new FormControl(null, [
+        Validators.required
+      ]),
+      'password': new FormControl(null, [
+        Validators.required
+      ])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+    this.form.reset();
+  }
+
+  validator(form: FormControl): boolean {
+    if (form.invalid && (form.dirty || form.touched)) {
+      return true;
+    }
+    return false;
+  }
+
+  isValid(form: FormGroup): boolean {
+    if (form.valid) {
+      return false;
+    }
+    return true;
   }
 
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  form;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      'fName': new FormControl(null, [
+        Validators.required
+      ]),
+      'lName': new FormControl(null, [
+        Validators.required
+      ]),
+      'email': new FormControl(null, [
+        Validators.required
+      ]),
+      'password': new FormControl(null, [
+        Validators.required
+      ]),
+      'cPassword': new FormControl(null, [
+        Validators.required
+      ])
+    });
   }
 
+  onSubmit() {
+
+  }
+
+  validator(form: FormControl) {
+    if (form.invalid && (form.dirty || form.touched)) {
+      return true;
+    }
+    return false;
+  }
+
+  isValid(form: FormGroup): boolean {
+    if (form.valid && !this.checkSamePassword()) {
+      return false;
+    }
+    return true;
+  }
+
+  checkSamePassword(): boolean {
+    if (this.form.get('password').value !== this.form.get('cPassword').value) {
+      return true;
+    }
+    return false;
+  }
 }
