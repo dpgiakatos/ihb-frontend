823ca4add8e0f489f10430e9f96f084f3efe0ccb
Θεμιστοκλής Χατζηεμμανουήλ <themis.chatzie@gmail.com>
2020-04-23 19:09:39 +0300

    Carousel improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'ihb-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private ngbCarousel: NgbCarouselConfig) { }
 
+  ngOnInit() {
+    this.ngbCarousel.pauseOnHover = false;
+  }
 }
