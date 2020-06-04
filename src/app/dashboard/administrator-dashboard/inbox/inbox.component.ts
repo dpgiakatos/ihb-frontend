b44aef7e085efeec0e216f92f021290d3c4725a6
nttzamos <nttzamos@csd.auth.gr>
2020-06-05 01:31:51 +0300

    Inbox and Contact Added

--- src/app/dashboard/administrator-dashboard/inbox/inbox.component.ts
+++ src/app/dashboard/administrator-dashboard/inbox/inbox.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactInbox, ContactInboxList } from './inbox.model';
+import { HttpClient } from '@angular/common/http';
+import { forkJoin } from 'rxjs';
+import { InboxService } from './inbox.service';
 
 @Component({
   selector: 'ihb-inbox',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InboxComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private inboxService: InboxService,
+    private httpClient: HttpClient
+  ) { }
+
+  inbox: ContactInbox[] = [];
+  limit = 10;
+  page = 1;
+  count: number;
 
   ngOnInit(): void {
+    this.fetchCurrentPage();
   }
 
+  fetchCurrentPage() {
+    forkJoin({
+      inboxList: this.httpClient.get<ContactInboxList>('contact/' + this.page),
+    }).subscribe(response => {
+      this.inbox = response.inboxList.contacts;
+      this.count = response.inboxList.count;
+    });
+  }
+
+  read(inboxMessage: ContactInbox) {
+
+  }
+
+  delete(inboxMessage: ContactInbox) {
+    this.httpClient.delete('contact/' + inboxMessage.id).subscribe(() => {
+      this.count--;
+      this.fetchCurrentPage();
+    })
+  }
 }
--- src/app/dashboard/administrator-dashboard/inbox/inbox.service.ts
+++ src/app/dashboard/administrator-dashboard/inbox/inbox.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ContactInbox } from './inbox.model';
+import { UrlSerializer } from '@angular/router';
 
 @Injectable()
 export class InboxService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private urlSerializer: UrlSerializer
+  ) { }
+
+  get(page: number) {
+    return this.httpClient.get<ContactInbox>('contact', { params: { page: page.toString() } });
+  }
 }
